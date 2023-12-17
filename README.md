42b0522b8ee92b3390becba6d58b734869b5a689
Rongeard Benoit <benoit.rongeard@gmail.com>
2023-12-17 14:07:09 +0100

    Fix dependencies + adding readme

---
+++ README.md
@@ -0,0 +1,38 @@
+# Personnal website
+
+A brief description of the project.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+```bash
+npm install
+```
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/) (v18.17.1)
+- [npm](https://www.npmjs.com/) (v9.6.7)
+- [live-server](https://www.npmjs.com/package/live-server) (v1.2.2)
+
+### Running the Server
+
+To run the server, follow these steps:
+
+1. Open a terminal.
+2. Navigate to the project directory.
+3. Run the command `npm run dev` to start the server.
+
+### Building for Production
+
+To build the project for production, follow these steps:
+
+1. Open a terminal.
+2. Navigate to the project directory.
+3. Run the command `npm run production` to build the project for production.
--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,94 @@
 // Requis
-var gulp = require('gulp'),
-    csscomb = require('gulp-csscomb'),
-    useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-clean-css');
+const gulp = require("gulp"),
+  csscomb = require("gulp-csscomb"),
+  useref = require("gulp-useref"),
+  gulpif = require("gulp-if"),
+  uglify = require("gulp-uglify"),
+  minifyCss = require("gulp-clean-css");
 
-var source = './node_modules';
-var destinationFonts = './app/resources/fonts';
-var destinationLibs = './app/resources/libs';
+const source = "./node_modules";
+const destinationFonts = "./app/resources/fonts";
+const destinationLibs = "./app/resources/libs";
 
 //Fonction pour inclure Font-Awesome
-gulp.task('fontAwesome', function(){
-    return gulp.src(source + '/font-awesome/fonts/*')
-        .pipe(gulp.dest(destinationFonts + '/font-awesome/'));
+gulp.task("fontAwesome", function () {
+  return gulp
+    .src(source + "/font-awesome/fonts/*")
+    .pipe(gulp.dest(destinationFonts + "/font-awesome/"));
 });
 
 //Fonction pour inclure Hover css
-gulp.task('hoverCss', function(){
-    return gulp.src(source + '/hover.css/css/hover.css')
-        .pipe(gulp.dest(destinationLibs + '/hover-css/'));
+gulp.task("hoverCss", function () {
+  return gulp
+    .src(source + "/hover.css/css/hover.css")
+    .pipe(gulp.dest(destinationLibs + "/hover-css/"));
 });
 
 //Fonction pour inclure Animate
-gulp.task('animateCss', function(){
-    return gulp.src(source + '/animate.css/animate.min.css')
-        .pipe(gulp.dest(destinationLibs + '/animate/'));
+gulp.task("animateCss", function () {
+  return gulp
+    .src(source + "/animate.css/animate.min.css")
+    .pipe(gulp.dest(destinationLibs + "/animate/"));
 });
 
 //Fonction pour inclure ProgressBar
-gulp.task('progressBar', function(){
-    return gulp.src(source + '/progressbar.js/dist/progressbar.min.js')
-        .pipe(gulp.dest(destinationLibs + '/progressbar/'));
+gulp.task("progressBar", function () {
+  return gulp
+    .src(source + "/progressbar.js/dist/progressbar.min.js")
+    .pipe(gulp.dest(destinationLibs + "/progressbar/"));
 });
 
 //Fonction pour inclure Jquery
-gulp.task('jquery', function(){
-    return gulp.src(source + '/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest(destinationLibs + '/jquery/'));
+gulp.task("jquery", function () {
+  return gulp
+    .src(source + "/jquery/dist/jquery.min.js")
+    .pipe(gulp.dest(destinationLibs + "/jquery/"));
 });
 
 //Fonction pour inclure JqueryInView
-gulp.task('jqueryInView', function(){
-    return gulp.src(source + '/jquery-inview/jquery.inview.min.js')
-        .pipe(gulp.dest(destinationLibs + '/jquery-inview/'));
+gulp.task("jqueryInView", function () {
+  return gulp
+    .src(source + "/jquery-inview/jquery.inview.min.js")
+    .pipe(gulp.dest(destinationLibs + "/jquery-inview/"));
 });
 
 //Fonction pour inclure PaceJs
-gulp.task('pace', function(){
-    return gulp.src(source + '/pace-progress/pace.min.js')
-        .pipe(gulp.dest(destinationLibs + '/pace/'));
+gulp.task("pace", function () {
+  return gulp
+    .src(source + "/pace-progress/pace.min.js")
+    .pipe(gulp.dest(destinationLibs + "/pace/"));
 });
 
 //Fonction pour lancer CSSComb
-gulp.task('csscomb', function() {
-    gulp.src('app/assets/scss/**')
-        .pipe(csscomb())
-        .pipe(gulp.dest('app/assets/scss'));
+gulp.task("csscomb", function () {
+  gulp
+    .src("app/assets/scss/**")
+    .pipe(csscomb())
+    .pipe(gulp.dest("app/assets/scss"));
 });
 
 //Fonction pour rassembler tous les JS, les CSS et les minifier
-gulp.task('useref', function () {
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(gulp.dest('app/'));
+gulp.task("useref", function () {
+  return gulp
+    .src("app/*.html")
+    .pipe(useref())
+    .pipe(gulpif("*.js", uglify()))
+    .pipe(gulpif("*.css", minifyCss()))
+    .pipe(gulp.dest("app/"));
 });
 
 // Tâches
-gulp.task('build', ['fontAwesome', 'hoverCss', 'animateCss', 'progressBar', 'jquery', 'jqueryInView', 'pace']);
-gulp.task('dev', ['csscomb']);
-gulp.task('production', ['useref']);
+gulp.task(
+  "build",
+  gulp.series(
+    "fontAwesome",
+    "hoverCss",
+    "animateCss",
+    "progressBar",
+    "jquery",
+    "jqueryInView",
+    "pace"
+  )
+);
+gulp.task("dev", gulp.series("csscomb"));
+gulp.task("production", gulp.series("useref"));
